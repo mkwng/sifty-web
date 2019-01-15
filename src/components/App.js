535c5ef4f09a02c537a24c3cf961ebba9500167a
mkwng <hello@mkwng.com>
2019-01-15 12:11:53 -0800

    ☝️ First commit!

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Button from 'antd/lib/button';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        App Test
+        <Button type="primary">Test button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
