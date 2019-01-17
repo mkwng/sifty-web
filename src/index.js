a7f8316e22d49d6b92be059d4b5e34276c8773c1
mkwng <hello@mkwng.com>
2019-01-16 17:18:58 -0800

    👥 User authentication

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
 import firebase from './firebase';
 
-import 'antd/dist/antd.css';
+// Components
+import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 
+// Styles
+import 'antd/dist/antd.css';
+import { Skeleton } from 'antd';
+
+// Router
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 
-// import { createStore } from 'redux';
-// import { Provider } from 'react-redux';
-// import { composeWithDevTools } from 'redux';
+// Redux
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser } from './actions';
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
   componentDidMount() {
+    console.log(this.props.isLoading);
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
+        this.props.setUser(user);
         this.props.history.push('/')
       }
     })
   }
 
   render() {
-    return (
+    return this.props.isLoading ? <Skeleton active /> : (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
@@ -34,11 +46,22 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const mapStateFromProps = state => ({
+  isLoading: state.user.isLoading
+});
+
+const RootWithAuth = withRouter(
+  connect(
+    mapStateFromProps, 
+    { setUser }
+  )(Root)
+);
 
 ReactDOM.render(
+  <Provider store={store}>
   <Router>
     <RootWithAuth />
   </Router>
+  </Provider>
   , document.getElementById('root'));
 registerServiceWorker();
